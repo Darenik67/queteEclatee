3fa79e9150c9bf3b22cc7627f52f78bc805cd702
darenik67 <karma-67.75@hotmail.fr>
2023-07-17 12:24:56 +0200

    first commit

---
+++ src/main/java/com/WildCodeSchool/queteEclatee/QueteEclateeApplication.java
@@ -0,0 +1,13 @@
+package com.WildCodeSchool.queteEclatee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueteEclateeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueteEclateeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WildCodeSchool/queteEclatee/QueteEclateeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WildCodeSchool.queteEclatee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueteEclateeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
